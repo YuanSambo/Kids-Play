5619fe1c9dafa65fed164f68dcf16302e1b31d72
Yuan Sambo <yuan.sambo@gmail.com>
2021-12-08 00:05:31 +0800

    Fix pop the balloons bug
    - Fix the bug where touch input registers more than one

--- Assets/Scripts/Game_Shapes/Balloon.cs
+++ Assets/Scripts/Game_Shapes/Balloon.cs
@@ -4,33 +4,12 @@ namespace Game_Shapes
 {
     public class Balloon : MonoBehaviour
     {
-        private Vector3 _screenPosition;
-        private Vector3 worldPosition;
+       
         public Shapes shape;
     
         private void Update()
         {
-            if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
-            {
-                Vector3 mousePosition = Input.GetTouch(0).position;
-                _screenPosition = new Vector2(mousePosition.x, mousePosition.y);
-                worldPosition = Camera.main.ScreenToWorldPoint(_screenPosition);
-
-                RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
-                if (hit.collider != null)
-                {
-                    if(shape.Equals(ShapeManager.Instance.shape))
-                    {
-                        GameManager.Instance.CorrectAnswer();
-                        Destroy(hit.transform.gameObject);
-                    }
-                    else
-                    {
-                        GameManager.Instance.WrongAnswer();
-                    }
-                }
-            }
-
+           
             if (transform.position.y > 8f)
             {
                 Destroy(gameObject);
---
+++ Assets/Scripts/Others/TouchController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Game_Shapes;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    private Vector3 _screenPosition;
+    private Vector3 worldPosition;
+
+    private void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Vector3 mousePosition = Input.GetTouch(0).position;
+            _screenPosition = new Vector2(mousePosition.x, mousePosition.y);
+            worldPosition = Camera.main.ScreenToWorldPoint(_screenPosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+            if (hit.collider != null)
+            {
+                var shape = hit.transform.GetComponent<Balloon>().shape;
+                if (shape == ShapeManager.Instance.shape)
+                {
+
+                    GameManager.Instance.CorrectAnswer();
+                    Destroy(hit.transform.gameObject);
+                }
+                else
+                {
+                    GameManager.Instance.WrongAnswer();
+                }
+            }
+        }
+
+    }
+
+}
